88dbb78e9695bb1806d7b9e714ca82d10369cf06
Viktor Svahn <svahnv@gmail.com>
2025-01-21 22:25:04 +0100

    initial commit

---
+++ src/xyzquery/main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+
+import sys
+import numpy as np
+from ase.io import read, iread, write
+
+#from xyzquery import utils
+import utils
+
+
+class Parse:
+	def __init__(self, path, query):
+		self.atoms = iread(path, ':')
+		self.query = query
+		
+		self.elements, self.config = self.query_interpreter()
+
+		# Makes a trailing ',' equivalent to ':or'
+		if '' in self.elements:
+			self.config += ['or']
+			self.elements.remove('')
+
+
+	def query_interpreter(self):
+		# Split query into elements and config strings
+		if ':' in self.query:
+			element_string, config_string = self.query.split(':')
+		else:
+			element_string, config_string = self.query, None
+
+		# Convert element string to element list
+		element_list = utils.string_to_list(element_string)
+		config_list = utils.string_to_list(config_string)
+
+		return element_list, config_list
+
+
+	def check_elements(self, structure, elements):
+		symbols = str(structure.symbols)
+
+		if 'not' in self.config:
+			if all(e not in symbols for e in elements):
+				return structure
+		else:
+			# Must include all elements
+			if ('and' in self.config) and all(e in symbols for e in elements):
+				return structure
+			# Must include either element
+			#elif ('or' in self.config) or ('' in self.elements):
+			elif 'or' in self.config:
+				if any(e in symbols for e in elements):
+					return structure
+			# Default behaviour
+			elif None in self.config:
+				# Given a list, defaults to 'or'
+				if len(elements)>1:
+					if any(e in symbols for e in elements):
+						return structure
+				# Given a single element, defaults to equivalence
+				elif elements[0] == symbols:
+					return structure
+
+
+	def find_structures(self):
+		query = map(lambda a: self.check_elements(a, self.elements), self.atoms)
+		new_atoms = filter(lambda a: a is not None, query)
+		return new_atoms
+
+
+if __name__ == '__main__':
+	# CLI arguments
+	path = str(sys.argv[1])
+	query = str(sys.argv[2])
+	try:
+		output = str(sys.argv[3])
+	except:
+		pass
+
+	# Parse input
+	parsed_query = Parse(path, query) # should generete a list that contains all matches
+	
+	# Printed search string summary. Should maybe be verbose output
+	utils.search_summary(parsed_query.config[0], parsed_query.elements)
+	
+
+	# Search
+	result = parsed_query.find_structures()
+	try:
+		write(output, result)
+	except:
+		i=0
+		for structure in result:
+			i+=1
+			print()
+			
+			E = structure.get_potential_energy()
+			F = structure.get_forces(); Fnorm = np.linalg.norm(F, axis=1)
+			print(f'Structure {i}:')
+			
+			# Print verbose
+			utils.print_info(structure, E, max(Fnorm), verbose=True)
+			#utils.print_info(structure, E, max(Fnorm))
+
+		print(f'\nNumber of hits: {i}')
---
+++ src/xyzquery/utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+
+def string_to_list(input_string):
+	if (input_string is not None) and (',' in input_string):
+		output_list = input_string.split(',')
+	else:
+		output_list = [input_string]
+	return output_list
+
+
+def search_summary(config, elements):
+	if config == 'not':
+		summary = 'not '+f' or '.join(elements)
+	elif config == 'and':
+		summary = f' and '.join(elements)
+	elif (config == 'or') or (config == None):
+		summary = f' or '.join(elements)
+	print(f'Searching for: {summary}')
+
+
+def print_info(structure, energy, fmax, verbose=False):
+	#print()
+	longest_key = max([len(s) for s in structure.info]+[16])
+	top_spaces = 4+longest_key
+	print(f'  Chemical formula{" "*(top_spaces-16)}{structure.symbols}')
+	print(f'  Energy{" "*(top_spaces-6)}{energy}')
+	print(f'  Fmax{" "*(top_spaces-4)}{fmax}')
+	
+	if verbose:
+		#print('  Info:')
+		for key,val in structure.info.items():
+			verbose_spaces = 4+longest_key-len(key)
+			print(f'  {key}{" "*verbose_spaces}{val}')
